4041aa9008d86df56315e67792343c1804b10cea
aceleradora-la <info@aceleradora.la>
2026-03-19 20:48:19 -0300

    Fix malformed PO translation file. Bump 1.0.4

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.0.3',
+    'version': '18.0.1.0.4',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
