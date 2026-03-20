2059bb727aab7d735afc19b18eec59a7fe63095a
aceleradora-la <info@aceleradora.la>
2026-03-20 09:19:48 -0300

    Fix active_id in product.template form view. Bump 1.0.9

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.0.8',
+    'version': '18.0.1.0.9',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
