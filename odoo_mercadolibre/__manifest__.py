b26b69a1d8f1151641733c82634b9cd628ecd2e5
aceleradora-la <info@aceleradora.la>
2026-03-21 17:17:15 -0300

    Temporarily remove chatter from meli.item to debug UI bug. Bump 1.2.1

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.2.0',
+    'version': '18.0.1.2.1',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
