d28cfb35ed7c03ae0d40782bfc8b2a619cc37a0e
aceleradora-la <info@aceleradora.la>
2026-03-21 18:57:52 -0300

    Add missing controllers and auth templates to tracking. Bump 1.4.1

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.4.0',
+    'version': '18.0.1.4.1',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
