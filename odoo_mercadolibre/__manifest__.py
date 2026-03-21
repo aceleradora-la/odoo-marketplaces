dfc78e2d3481d48b63e8eb650be000e5a8a5e62e
aceleradora-la <info@aceleradora.la>
2026-03-21 16:56:55 -0300

    Switch back to mandatory list tags for Odoo 18/19. Bump 1.2.0

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.1.0',
+    'version': '18.0.1.2.0',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
