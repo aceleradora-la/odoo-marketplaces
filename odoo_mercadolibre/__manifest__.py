f66d64235361885be3cc8495778f9d9ca7a78307
aceleradora-la <info@aceleradora.la>
2026-03-23 12:26:29 -0300

    Improve image URLs and add Close/Delete actions to Items. Bump 1.5.0

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.4.9',
+    'version': '18.0.1.5.0',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
