995b7397b5ff73ce817993940ae598331520cf26
aceleradora-la <info@aceleradora.la>
2026-03-23 11:59:45 -0300

    Fix missing http import in controllers. Bump 1.4.9

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.4.8',
+    'version': '18.0.1.4.9',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
