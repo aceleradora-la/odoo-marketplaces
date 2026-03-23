3f18e3b10bfa9889d1b35d263420e24885e65730
aceleradora-la <info@aceleradora.la>
2026-03-23 10:14:56 -0300

    Fix token logic and syntax error. Added stripping and UserError visibility. Bump 1.4.6

--- odoo_mercadolibre/__manifest__.py
+++ odoo_mercadolibre/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'MercadoLibre Integration',
-    'version': '18.0.1.4.5',
+    'version': '18.0.1.4.6',
     'category': 'Sales',
     'summary': 'Integración completa entre Odoo y MercadoLibre (V18/19)',
     'description': """
